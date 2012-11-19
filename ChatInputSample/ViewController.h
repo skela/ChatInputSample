12aeddb5e34f85c2299541fd8d8ecf0e525c1f64
Marat Alekperov <ma@grape.me>
2012-11-20 00:33:16 +0400

    Initial Commit

---
+++ ChatInputSample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ChatInputSample
+//
+//  Created by Office on 20.11.12.
+//  Copyright (c) 2012 Me and Myself. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ChatInputSample/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ChatInputSample
+//
+//  Created by Office on 20.11.12.
+//  Copyright (c) 2012 Me and Myself. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
